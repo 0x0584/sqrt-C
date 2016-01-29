119c337904ccafa273152760f013b875160d71ce
Anas Rchid <anasrchid@users.noreply.github.com>
2016-01-29 19:53:14 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,10 +5,10 @@
  #define KYEL  "\x1B[33m"
  #define KBLU  "\x1B[34m" 
 int main (int argc, char *argv[]) {
-	float val, min, max; 
-	float mid;//mid is sqrt()
-	float S, _mid;
-	int step = 0;
+	double val, min, max; 
+	double sqrt;
+	double S, _sqrt;
+	int cnt = 0;
 	
 	// enter an argument,
 	
@@ -17,21 +17,29 @@ int main (int argc, char *argv[]) {
 		return 1;
 	}
 	    val = fabs (atof (argv[1]));// <force to non-negative.>
-		min = 0;//minimum 
-		max = mid = val;//maximum 
-		_mid = 1;
+	if ( val < 1.0 )
+		{
+		min = val;        
+		max = sqrt = 1.0; // if val < 1.0 result is greater than val but less than 1.0
+		}
+	 else
+	{
+		min = 0;          //minimum 
+	        max = sqrt = val; //maximum
+	} 
 		//1e-8 = 10^(-8) = 0.00000001
-		while (fabs(_mid - mid) >= 1e-8) {
-			_mid = mid;//keep the old sqrt()
-			mid = (max + min) / 2;
-			S = mid * mid;//S is mid^(2)
-			printf("test n(%d)\tsqrt(%f) = %.6f\t",++step,val, mid);
+		do
+		{
+		_sqrt = sqrt;//keep the old sqrt
+	        sqrt = (max + min) / 2;
+		S = sqrt * sqrt;
+			printf("test n(%d)\tsqrt(%f) = %.6f\t",++cnt,val, sqrt);
 			if (S > val) {
 				puts("too high");
 				printf(KYEL);
 				printf("\t\t=%s %f\n","the old max",max);
 				printf("\t\t=%s %f\n","the old min",min);
-				max = mid;//setting max to the current mid
+				max = sqrt;//setting max to the current mid
 				printf("\t\t>%4s %f","the new max",max);
 				printf(RESET);
 			} else {
@@ -39,16 +47,16 @@ int main (int argc, char *argv[]) {
 				printf(KBLU);
 				printf("\t\t-%s %f\n","the old max",max);
 				printf("\t\t-%s %f\n","the old min",min);
-				min = mid;//setting min to the current mid
+				min = sqrt;//setting min to the current mid
 				printf("\t\t>%4s %f","the new min",min);
 				printf(RESET);
 			}//end else
 			puts("");
-		}//end while
+		}while (fabs(_sqrt - sqrt) >= 1e-8);
 		puts("");
 		printf(KRED);
 		puts("\t====================================================");
-		printf ("\tfinal value.\tsqrt(%f) = %.6f\n", val, mid);
+		printf ("\tfinal value.\tsqrt(%f) = %.6f\n", val, sqrt);
 		puts("\t====================================================");
 		printf(RESET);
 	return 0;
